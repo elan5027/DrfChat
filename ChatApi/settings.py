227e376bf0acdbf93cb2e5671aa8b1ca280a0857
Park Jihong <elan5027@naver.com>
2023-06-02 18:58:43 +0900

    testgit add .

--- ChatApi/settings.py
+++ ChatApi/settings.py
@@ -184,7 +184,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOG_DIR = os.path.join(BASE_DIR, 'log')
+LOG_FILE = '/api.log'
+LOG_PATH = LOG_DIR + LOG_FILE
 
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+if not os.path.exists(LOG_PATH):
+    f = open(LOG_PATH, 'a').close()  # create empty log file
+else:
+    f = open(LOG_PATH, "w").close()  # clear log file
 
 LOGGING = {
     'version': 1,
@@ -193,7 +203,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',                           # 로그 레벨
             'class': 'logging.FileHandler',
-            'filename': '/path/to/django/debug.log',    # 로그 경로
+            'filename': LOG_PATH,    # 로그 경로
         },
     },
     'root': {
