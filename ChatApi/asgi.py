f0cd7a9086ee0ffa5c334708b91a20872d03bfe4
Park Jihong <elan5027@naver.com>
2023-06-02 18:17:01 +0900

    testgit add .

--- ChatApi/asgi.py
+++ ChatApi/asgi.py
@@ -2,7 +2,6 @@ import django
 import os
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApi.settings")
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
@@ -20,4 +19,3 @@ application = ProtocolTypeRouter({
 })
 
 
-django.setup()
