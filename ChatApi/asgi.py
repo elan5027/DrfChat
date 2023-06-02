117fe81e3bab78b7382d1ecd291e7e02a842d11e
Park Jihong <elan5027@naver.com>
2023-06-02 17:21:49 +0900

    jenkins_requtest_lastgit add .

--- ChatApi/asgi.py
+++ ChatApi/asgi.py
@@ -1,20 +1,15 @@
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApi.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.channelsmiddleware import TokenAuthMiddleware
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApi.settings')
+
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
-    # 'websocket':AuthMiddlewareStack(
-    #     URLRouter(
-    #         chat.routing.websocket_urlpatterns
-    #     )
-    # ),
     'websocket':TokenAuthMiddleware(
         URLRouter(
             chat.routing.websocket_urlpatterns
