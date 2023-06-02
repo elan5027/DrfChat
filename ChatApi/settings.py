4da5179400cb08dfce4f0b02f874c7888176043b
Park Jihong <elan5027@naver.com>
2023-06-02 17:03:48 +0900

    jenkins_requtest_last

--- ChatApi/settings.py
+++ ChatApi/settings.py
@@ -182,3 +182,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApi.settings')
